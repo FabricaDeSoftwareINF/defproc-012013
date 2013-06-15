5fdaa09ae2930d68c54d3d5718d25eddf2a9ac8d
natandmenor@gmail.com <natandmenor@gmail.com@0fd97b3d-6a56-fa6d-1cfc-7d1f5ae6880c>
2013-06-15 00:06:29 +0000

    Criação do projeto na estrutura do Maven

---
+++ trunk/Desenvolvimento/Projeto/defproc/src/main/java/br/ufg/inf/fs/defproc/App.java
@@ -0,0 +1,13 @@
+package br.ufg.inf.fs.defproc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
