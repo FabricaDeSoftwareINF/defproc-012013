11fb1d44c0755408068671edb719d78f53b84bdf
jonathan.engsoftware@gmail.com <jonathan.engsoftware@gmail.com@0fd97b3d-6a56-fa6d-1cfc-7d1f5ae6880c>
2013-07-11 23:36:24 +0000

    AbstractController

--- trunk/Desenvolvimento/Projeto/projeto_web/defproc/src/java/br/ufg/inf/fs/defproc/controller/AbstractController.java
+++ trunk/Desenvolvimento/Projeto/projeto_web/defproc/src/java/br/ufg/inf/fs/defproc/controller/AbstractController.java
@@ -1,14 +1,51 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.ufg.inf.fs.defproc.controller;
 
+import br.ufg.inf.fs.defproc.domain.Atividade;
+import br.ufg.inf.fs.defproc.domain.FerramentasDaAtividade;
+
 /**
- *
- * @author Jonathan
+ * Classe abstrata que define o controlador de atividades no modelo MVC
+ * Controller é o componente intermediário entre a apresentação (View) e os componentes de negócio (Serviços + DAO + Model).
+ * 
+ * @author Jonathan Silva Pereira
+ * 
+ * 11/07/2013
  */
+
 public abstract class AbstractController {
+    
+    private Atividade atividade;
+
+    public AbstractController(Atividade atividade) {
+        this.atividade = atividade;
+    }
+    
+    public void setName(String name) {
+        this.atividade.setNome(name);
+    }
+    
+    public void setDescricao(String descricao) {
+        this.atividade.setDescricao(descricao);
+    }
+    
+    public void addFerramenta(FerramentasDaAtividade ferramenta) {
+        //this.atividade.listaFerramentas.add(ferramenta);
+    }
+    
+    public void setCriterioEntrada() {
+        
+    }
+    
+    public void setCriterioSaida() {
+        
+    }
+    
+    public Atividade getAtividade() {
+        return atividade;
+    }
+    
+    
+    
     
     
 }
